7b52b730a9c046fcf8e5d2f1c03018451e1d43c4
paulamb96 <75475999+paulamb96@users.noreply.github.com>
2025-09-22 18:15:29 +0000

    adding initial layout of python project

---
+++ hello.py
@@ -0,0 +1,16 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+##Probamos 'make lint'
+
+# #This is nonsense coda that generates a warnisg
+# var = 1
+# var = var
+
+# #Bad syntax
+# foo =
